e116a50fb5c6d428250a931c499d8e7aefeeeed0
Graham H. Edwards <grahamhedwards92@gmail.com>
2023-04-16 12:27:30 -0400

    clean up decay constants and add dois

--- decay-constants.jl
+++ decay-constants.jl
@@ -1,9 +1,18 @@
-## Decay Constants
+## Decay Constants, in a⁻¹
 
-const l238 = log(2)/ 4.4683e9 # 1/a; Jaffey et al. 1971, Phys. Rev. C
-const l234 = log(2)/ 245620 # 1/a; Cheng et al. 2013, EPSL
-const l230 = log(2)/ 75584 # "  "
-const l232 = log(2)/ 14.05e9 #1/a Audi et al. 1997, Nuc. Phys. A
-const l226 = log(2)/ 1599 # 1/a; Holden, 1990, Pure and Applied Chem.
-const SEU = l238/l234
-const SEThU = l238/l230
+# ²³⁸U --- Jaffey et al. 1971, Phys. Rev. C, doi: 10.1103/PhysRevC.4.1889
+const l238 = log(2)/ 4.4683e9
+
+# ²³⁴U, ²³⁰Th --- Cheng et al. 2013, EPSL, doi: 10.1016/j.epsl.2013.04.006
+const l234 = log(2)/ 245620 
+const l230 = log(2)/ 75584
+
+# ²³²Th --- Audi et al. 1997, Nuc. Phys. A, doi: 10.1016/S0375-9474(97)00482-X
+const l232 = log(2)/ 14.05e9
+
+# ²²⁶Ra --- Holden, 1990, Pure and Applied Chem., doi: 10.1351/pac199062050941
+const l226 = log(2)/ 1599 
+
+## Secular equilibrium values based on decay constants:
+const seU = l238/l234
+const seThU = l238/l230
